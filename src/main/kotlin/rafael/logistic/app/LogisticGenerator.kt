073b73e237d535d80c238ed10a051048560d0023
Rafael Afonso <rafonso@magnasistemas.com.br>
2020-02-12 14:23:54 -0300

    plotando os dados.

---
+++ src/main/kotlin/rafael/logistic/app/LogisticGenerator.kt
@@ -0,0 +1,55 @@
+package rafael.logistic.app
+
+import java.time.Instant
+
+sealed class LogisticEvent() {
+    abstract val interaction: Int
+    abstract val priorX: Double
+    abstract val x: Double
+    val instant = Instant.now()
+}
+
+data class StartingEvent(override val x: Double) : LogisticEvent() {
+    override val interaction: Int = 0
+    override val priorX: Double = Double.NaN
+}
+
+data class RunningEvent(override val interaction: Int, override val priorX: Double, override val x: Double) : LogisticEvent()
+
+data class EndingEvent(override val interaction: Int, override val priorX: Double, override val x: Double) : LogisticEvent()
+
+typealias LogisticEventListener = (LogisticEvent) -> Unit
+
+class LogisticGenerator() {
+
+    private val listeners = mutableListOf<LogisticEventListener>()
+
+    private fun notify(event: LogisticEvent) {
+        listeners.forEach { it(event) }
+    }
+
+    private tailrec fun iterate(r: Double, interactions: Int, interaction: Int, xPrior: Double, values: List<Double>): List<Double> {
+        val x = xPrior * r * (1.0 - xPrior)
+        if (interaction == interactions) {
+            notify(EndingEvent(interaction, xPrior, x))
+            return values + x
+        }
+
+        notify(RunningEvent(interaction, xPrior, x))
+        return iterate(r, interactions, interaction + 1, x, values + x)
+    }
+
+    fun generate(x0: Double, r: Double, interactions: Int): List<Double> {
+        notify(StartingEvent(x0))
+        return iterate(r, interactions, 1, x0, listOf(x0))
+    }
+
+    fun addStatusListener(listener: LogisticEventListener) {
+        listeners.add(listener)
+    }
+
+    fun removeStatusListener(listener: LogisticEventListener) {
+        listeners.remove(listener)
+    }
+
+}
--- src/main/kotlin/rafael/logistic/view/MainView.kt
+++ src/main/kotlin/rafael/logistic/view/MainView.kt
@@ -1,6 +1,7 @@
 package rafael.logistic.view
 
 import javafx.beans.property.IntegerProperty
+import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleIntegerProperty
 import javafx.event.Event
 import javafx.geometry.Pos
@@ -11,10 +12,16 @@ import javafx.scene.control.Spinner
 import javafx.scene.control.SpinnerValueFactory
 import javafx.scene.input.*
 import javafx.scene.layout.BorderPane
+import javafx.scene.shape.Circle
+import javafx.scene.shape.Line
+import javafx.scene.shape.Shape
 import javafx.util.StringConverter
+import rafael.logistic.app.*
 import tornadofx.*
 import java.math.RoundingMode
 import java.text.DecimalFormat
+import java.time.Duration
+import java.time.Instant
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.pow
@@ -23,6 +30,8 @@ private const val MAX_DELTA = 0.1
 private const val MIN_STEP = 1
 private const val MAX_STEP = 7
 private const val X_STEPS = 100
+
+typealias Coord = Pair<Double, Double>
 
 class MainView : View("Logistic Equation") {
 
@@ -37,17 +46,29 @@ class MainView : View("Logistic Equation") {
     // @formatter:on
 
     // @formatter:off
-    private val deltaRProperty          =   SimpleIntegerProperty(this, "deltaR"     , 1        )
+    private val deltaRProperty          =   SimpleIntegerProperty(this, "deltaR"    , 1     )
     private val rValueFactory           =   SpinnerValueFactory.DoubleSpinnerValueFactory(0.0, 4.0, 1.0, MAX_DELTA)
 
-    private val deltaX0Property         =   SimpleIntegerProperty(this, "deltaX0"    , 1        )
+    private val deltaX0Property         =   SimpleIntegerProperty(this, "deltaX0"   , 1     )
     private val x0ValueFactory          =   SpinnerValueFactory.DoubleSpinnerValueFactory(0.0, 1.0, 0.5, MAX_DELTA)
 
     private val iteractionsValueFactory =   SpinnerValueFactory.IntegerSpinnerValueFactory(50, 2000, 100, 50)
 
+    private val generator               =   LogisticGenerator()
+
+    private val runningProperty         =   SimpleBooleanProperty(this, "running"   , false )
+
+    private var t0: Instant?            =   null
+
+    private val logisticChartBackgound  =   logisticChart.lookup(".chart-plot-background")
+
+    private val logisticData            =   emptyList<Double>().toProperty()
+
     // @formatter:on
 
     init {
+        generator.addStatusListener(this::dataGenerated)
+
         spnR.valueFactory = rValueFactory
         initScrollSpinner(spnR)
         initCtrlMouseSpinner(spnR, deltaRProperty)
@@ -56,6 +77,7 @@ class MainView : View("Logistic Equation") {
         spnX0.valueFactory = x0ValueFactory
         initScrollSpinner(spnX0)
         initCtrlMouseSpinner(spnX0, deltaX0Property)
+        spnX0.valueProperty().onChange { loadData() }
 
         spnIteractions.valueFactory = iteractionsValueFactory
         initScrollSpinner(spnIteractions)
@@ -63,29 +85,54 @@ class MainView : View("Logistic Equation") {
         spnIteractions.valueProperty().addListener { _, _, newValue ->
             iteractionsXAxis.upperBound = newValue.toDouble()
             iteractionsXAxis.tickUnit = newValue.toDouble() / 10
+            loadData()
         }
 
-
-
-        (logisticChart.xAxis    as NumberAxis).tickLabelFormatter = SpinnerConverter(2) as StringConverter<Number>
-        (logisticChart.yAxis    as NumberAxis).tickLabelFormatter = SpinnerConverter(2) as StringConverter<Number>
-
+        (logisticChart.xAxis as NumberAxis).tickLabelFormatter = SpinnerConverter(2) as StringConverter<Number>
+        (logisticChart.yAxis as NumberAxis).tickLabelFormatter = SpinnerConverter(2) as StringConverter<Number>
         (iteractionsChart.yAxis as NumberAxis).tickLabelFormatter = SpinnerConverter(2) as StringConverter<Number>
 
+        logisticChart.boundsInParentProperty().onChange { refreshLogisticPlot() }
+
+        logisticData.also { it.onChange { reloadPlots() } }
         loadData()
     }
 
-    private fun loadData() {
-        logisticChart.data.clear()
-        logisticChart.series("xy") {
-            cssclass("line1")
-            data = listOf(data(0.0, 0.0), data(1.0, 1.0)).observable()
-        }
-        logisticChart.series("parable") {
-            data = (0..X_STEPS).map { it.toDouble() / X_STEPS }
-                    .map { XYChart.Data(it, it * spnR.value * (1.0 - it)) }.observable()
-        }
-    }
+    private fun Double.toLogisticXPos() = logisticChart.xAxis.getDisplayPosition(this)
+
+    private fun Double.toLogisticYPos() = logisticChart.yAxis.getDisplayPosition(this)
+
+    private tailrec fun generateLogisticData(itValues: Iterator<Double>, pt1: Pair<Double, Double>, shapes: List<Shape>): List<Shape> =
+            if (!itValues.hasNext()) shapes // .also { println(it) }
+            else {
+                val pt2 = Pair(pt1.second, itValues.next())
+
+                val c1 = Circle(pt1.first.toLogisticXPos(), pt1.second.toLogisticYPos(), 2.0).also {
+                    it.fill = c("green")
+                }
+                val c2 = Circle(pt2.first.toLogisticXPos(), pt2.first.toLogisticYPos(), 2.0).also {
+                    it.fill = c("blue")
+                }
+                generateLogisticData(itValues, pt2, shapes + c1 + c2)
+            }
+
+    private tailrec fun coordToShape(itCoord: ListIterator<Coord>, priorCoord: Coord, shapes: List<Shape>): List<Shape> =
+            if (!itCoord.hasNext()) shapes
+            else {
+                val strColor = if (itCoord.nextIndex() % 2 == 0) "green" else "blue"
+                val coord = itCoord.next()
+                val c2 = Circle(coord.first.toLogisticXPos(), coord.first.toLogisticYPos(), 2.0).also {
+                    it.fill = c(strColor)
+                }
+                coordToShape(itCoord, coord, shapes + c2)
+            }
+
+    private tailrec fun dataToCoord(itData: Iterator<Double>, priorX: Double, pairs: List<Coord>): List<Coord> =
+            if (!itData.hasNext()) pairs
+            else {
+                val x = itData.next()
+                dataToCoord(itData, x, pairs + Pair(priorX, x) + Pair(x, x))
+            }
 
     private fun initScrollSpinner(spinner: Spinner<*>) {
         spinner.setOnScroll { event ->
@@ -145,6 +192,57 @@ class MainView : View("Logistic Equation") {
         }
     }
 
+    private fun dataGenerated(event: LogisticEvent) {
+        when (event) {
+            is StartingEvent -> t0 = event.instant
+            is RunningEvent -> {
+
+            }
+            is EndingEvent -> {
+                val t1 = Instant.now()
+                val deltaT = Duration.between(t0, t1)
+                println(deltaT)
+            }
+        }
+    }
+
+    private fun loadData() {
+        this.logisticData.value = generator.generate(spnX0.value, spnR.value, spnIteractions.value)
+    }
+
+
+    private fun reloadPlots() {
+        logisticChart.data.clear()
+        logisticChart.series("xy") {
+            cssclass("line1")
+            data = listOf(data(0.0, 0.0), data(1.0, 1.0)).observable()
+        }
+        logisticChart.series("parable") {
+            data = (0..X_STEPS).map { it.toDouble() / X_STEPS }
+                    .map { XYChart.Data(it, it * spnR.value * (1.0 - it)) }.observable()
+        }
+        refreshLogisticPlot()
+
+        iteractionsChart.data.clear()
+        iteractionsChart.series("iteractions") {
+            data = logisticData.value.mapIndexed { index, d -> XYChart.Data(index, d) }.observable()
+        }
+    }
+
+    private fun refreshLogisticPlot() {
+        logisticChartBackgound.replaceChildren()
+        runLater {
+            val data = logisticData.value
+            val coords = (listOf(Pair(data[0], 0.0)) + (1 until data.size)
+                    .flatMap { i -> listOf(Pair(data[i - 1], data[i]), Pair(data[i], data[i])) })
+                    .map { (x, y) -> Pair(x.toLogisticXPos(), y.toLogisticYPos()) }
+            (1 until coords.size)
+                    .map { i -> Line(coords[i - 1].first, coords[i - 1].second, coords[i].first, coords[i].second).also { l ->
+                        l.stroke = if(i % 2 == 0) c("green") else c("blue")
+                    } }
+                    .forEach { l -> logisticChartBackgound.add(l) }
+        }
+    }
 
 }
 
