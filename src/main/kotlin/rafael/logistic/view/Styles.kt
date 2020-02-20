58148d38cf17b237154805d4c91185ba2d6946bd
Rafael Afonso <rafonso@magnasistemas.com.br>
2020-02-20 09:22:34 -0300

    Estilo CSS para o Spinner

--- src/main/kotlin/rafael/logistic/view/Styles.kt
+++ src/main/kotlin/rafael/logistic/view/Styles.kt
@@ -1,31 +1,17 @@
 package rafael.logistic.view
 
-import javafx.scene.text.FontWeight
-import tornadofx.Stylesheet
-import tornadofx.box
-import tornadofx.cssclass
+import javafx.scene.paint.Color
 import tornadofx.*
 
 class Styles : Stylesheet() {
     companion object {
-        val heading by cssclass()
-        val parable by cssclass()
-        val series0 by cssclass()
     }
 
     init {
-        label and heading {
-            padding = box(10.px)
-            fontSize = 20.px
-            fontWeight = FontWeight.BOLD
-        }
-        parable {
-            stroke = c("violet")
-            strokeWidth  = 2.px
-        }
-        series0 {
-            stroke = c("gold")
-            strokeWidth  = 2.px
+        spinner and focused {
+            textInput {
+                backgroundColor += Color.KHAKI
+            }
         }
     }
 }
