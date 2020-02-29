d5d6c437e80051431efa09df2b67360d84491210
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2020-02-29 19:21:24 -0300

    Refatoração da Configuração dos spinners.
    Também correção na mudança de valor ao teclar Ctrl + RIGHT.

--- src/main/kotlin/rafael/logistic/view/Spinners.kt
+++ src/main/kotlin/rafael/logistic/view/Spinners.kt
@@ -12,52 +12,53 @@ import java.text.DecimalFormat
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.pow
+import kotlin.reflect.KFunction1
 
 private const val MIN_STEP = 1
 private const val MAX_STEP = 9
 
-private fun Spinner<*>.configureSpinnerIncrement() {
-    this.setOnScroll { event ->
-        val delta = if (event.isControlDown) 10 else 1
+private fun IntegerProperty.incrementConditional() {
+    this.value = min(MAX_STEP, this.value + 1)
+}
 
-        if (event.deltaY > 0) this.increment(delta)
-        if (event.deltaY < 0) this.decrement(delta)
-    }
-    this.addEventHandler(KeyEvent.KEY_PRESSED) { event ->
-        if (event.isControlDown) {
-            if (event.code == KeyCode.UP) {
-                this.increment(10)
-            } else if (event.code == KeyCode.DOWN) {
-                this.increment(-10)
+private fun IntegerProperty.decrementConditional() {
+    this.value = max(MIN_STEP, this.value - 1)
+}
+
+private val incrementAction: Map<Enum<*>, KFunction1<IntegerProperty, Unit>> = mapOf(
+        // @formatter:off
+        Pair(KeyCode    .RIGHT      , IntegerProperty::incrementConditional),
+        Pair(KeyCode    .LEFT       , IntegerProperty::decrementConditional),
+        Pair(MouseButton.PRIMARY    , IntegerProperty::decrementConditional),
+        Pair(MouseButton.SECONDARY  , IntegerProperty::incrementConditional)
+        // @formatter:on
+)
+
+private fun Spinner<*>.incrementValue(event: Event) {
+    when (event) {
+        is ScrollEvent -> {
+            val delta = if (event.isControlDown) 10 else 1
+            if (event.deltaY > 0) this.increment(delta)
+            if (event.deltaY < 0) this.decrement(delta)
+        }
+        is KeyEvent    -> {
+            if (event.eventType == KeyEvent.KEY_PRESSED) {
+                if (event.isControlDown) {
+                    if (event.code == KeyCode.UP) {
+                        this.increment(10)
+                    } else if (event.code == KeyCode.DOWN) {
+                        this.decrement(10)
+                    }
+                }
             }
         }
     }
 }
 
-private fun Spinner<Double>.configureSpinnerStep(stepProperty: IntegerProperty) {
-    // Desabilita o Context Menu. Fonte: https://stackoverflow.com/questions/43124577/how-to-disable-context-menu-in-javafx
-    this.addEventFilter(ContextMenuEvent.CONTEXT_MENU_REQUESTED, Event::consume)
-    this.addEventFilter(MouseEvent.MOUSE_CLICKED) { event ->
-        if (event.isControlDown) {
-            if (event.button == MouseButton.PRIMARY) {
-                stepProperty.value = max(MIN_STEP, stepProperty.value - 1)
-            } else if (event.button == MouseButton.SECONDARY) {
-                stepProperty.value = min(MAX_STEP, stepProperty.value + 1)
-            }
-        }
+private fun Spinner<Double>.handleIncrement(isControl: Boolean, enum: Enum<*>, stepProperty: IntegerProperty) {
+    if (isControl) {
+        incrementAction[enum]?.let { it(stepProperty) }
     }
-    this.addEventHandler(KeyEvent.KEY_PRESSED) { event ->
-        if (event.isControlDown) {
-            if (event.code == KeyCode.RIGHT) {
-                stepProperty.value = min(MAX_STEP, stepProperty.value + 1)
-            } else if (event.code == KeyCode.LEFT) {
-                stepProperty.value = min(MAX_STEP, stepProperty.value - 1)
-            }
-        }
-    }
-
-    stepProperty.addListener(ChangeListener { _, _, newStep -> this.stepChanged(newStep.toInt()) })
-    this.stepChanged(stepProperty.value)
 }
 
 private fun Spinner<Double>.stepChanged(step: Int) {
@@ -76,14 +77,21 @@ private fun Spinner<Double>.stepChanged(step: Int) {
 
 private fun Spinner<*>.bind(valueFactory: SpinnerValueFactory<*>, action: () -> Unit) {
     this.valueFactory = valueFactory
-    this.configureSpinnerIncrement()
+    this.addEventHandler(ScrollEvent.SCROLL, this::incrementValue)
+    this.addEventHandler(KeyEvent.KEY_PRESSED, this::incrementValue)
     this.valueProperty().onChange { action() }
 }
 
 fun Spinner<Double>.configureActions(valueFactory: SpinnerValueFactory.DoubleSpinnerValueFactory,
                                      deltaProperty: IntegerProperty, action: () -> Unit) {
     this.bind(valueFactory, action)
-    this.configureSpinnerStep(deltaProperty)
+
+    // Desabilita o Context Menu. Fonte: https://stackoverflow.com/questions/43124577/how-to-disable-context-menu-in-javafx
+    this.addEventFilter(ContextMenuEvent.CONTEXT_MENU_REQUESTED, Event::consume)
+    this.addEventFilter(MouseEvent.MOUSE_CLICKED) { this.handleIncrement(it.isControlDown, it.button, deltaProperty) }
+    this.addEventHandler(KeyEvent.KEY_PRESSED) { this.handleIncrement(it.isControlDown, it.code, deltaProperty) }
+    deltaProperty.addListener(ChangeListener { _, _, newStep -> this.stepChanged(newStep.toInt()) })
+    this.stepChanged(deltaProperty.value)
 }
 
 fun Spinner<Int>.configureActions(valueFactory: SpinnerValueFactory.IntegerSpinnerValueFactory, action: () -> Unit) {
