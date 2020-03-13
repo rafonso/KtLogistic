abb58ffa96b3c73d8a7b8a7d40ded13e67a4c785
Rafael Afonso <rafonso@magnasistemas.com.br>
2020-03-13 14:39:31 -0300

    Parametrização de valores.

--- src/main/kotlin/rafael/logistic/maps/bifurcation/ConvergenceType.kt
+++ src/main/kotlin/rafael/logistic/maps/bifurcation/ConvergenceType.kt
@@ -1,7 +1,8 @@
 package rafael.logistic.maps.bifurcation
 
-const val END_CONSTANT = 2.999
+const val END_CONSTANT = 3.00 - 0.00
 const val END_CYCLE_2 = 3.449489742783178 // = 1.0 + sqrt(6)
+const val END_VALUE = 4.000000000001
 
 enum class ConvergenceType {
     ZERO, CONSTANT, CYCLE_2, CHAOS;
@@ -13,7 +14,7 @@ enum class ConvergenceType {
                     in 0.0..1.0                  -> ZERO
                     in 1.0..END_CONSTANT         -> CONSTANT
                     in END_CONSTANT..END_CYCLE_2 -> CYCLE_2
-                    in END_CYCLE_2..4.0          -> CHAOS
+                    in END_CYCLE_2..END_VALUE    -> CHAOS
                     else                         -> error("Invalid r: $r")
                 }
 
