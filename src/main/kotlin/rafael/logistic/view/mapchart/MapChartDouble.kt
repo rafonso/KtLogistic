4c5d9c9eeaf715f59743660a53f835e7d81d1685
Rafael Afonso <rafonso@magnasistemas.com.br>
2020-03-17 14:31:32 -0300

    Refatoração de MapChart e implementações.
    União da "plotagem" MapChartBase com BifurcationCanvas.

--- src/main/kotlin/rafael/logistic/view/mapchart/MapChartDouble.kt
+++ src/main/kotlin/rafael/logistic/view/mapchart/MapChartDouble.kt
@@ -2,6 +2,7 @@ package rafael.logistic.view.mapchart
 
 import javafx.beans.NamedArg
 import javafx.collections.ObservableList
+import javafx.scene.Node
 import javafx.scene.chart.Axis
 import javafx.scene.paint.Color
 import javafx.scene.shape.Line
@@ -21,9 +22,6 @@ abstract class MapChartDouble(
     constructor(@NamedArg("xAxis") xAxis: Axis<Double>, @NamedArg("yAxis") yAxis: Axis<Double>) :
             this(xAxis, yAxis, mutableListOf<Series<Double, Double>>().observable())
 
-//    val x0Property = square.xProperty
-//    var x0 by x0Property
-
     private fun plotWithLines() {
         val coords = (listOf(Pair(data[0], 0.0)) + (1 until data.size)
                 .flatMap { i -> listOf(Pair(data[i - 1], data[i]), Pair(data[i], data[i])) })
@@ -41,18 +39,21 @@ abstract class MapChartDouble(
     }
 
     protected fun plotLines(coords: List<Pair<Double, Double>>, handler: (Line, Int) -> Unit = { _, _ -> }) {
-        val elements = (1 until coords.size)
-                .toList()
-                .parallelStream()
-                .map { i ->
-                    Line(coords[i - 1].first, coords[i - 1].second, coords[i].first, coords[i].second).also { l ->
-                        handler(l, i)
-                    }
-                }
-                .collect(Collectors.toList())
+        val elements = coordinatesToLines(coords, handler)
 
         background.getChildList()?.addAll(elements)
     }
+
+    private fun coordinatesToLines(coords: List<Pair<Double, Double>>, handler: (Line, Int) -> Unit): List<Line> =
+            (1 until coords.size)
+                    .toList()
+                    .parallelStream()
+                    .map { i ->
+                        Line(coords[i - 1].first, coords[i - 1].second, coords[i].first, coords[i].second).also { l ->
+                            handler(l, i)
+                        }
+                    }
+                    .collect(Collectors.toList())
 
     protected abstract fun recalculateBounds()
 
@@ -64,22 +65,30 @@ abstract class MapChartDouble(
         super.prefWidthProperty().bindBidirectional(super.prefHeightProperty())
     }
 
-    override fun plotData() {
+    override fun prepareBackground() {
+        background.getChildList()?.clear()
         recalculateBounds()
         refreshXY()
         refreshAsymptote()
-        if (data.isNotEmpty()) {
-            // Destaca o x0 e y0
-            highlightP0(data.first(), 0.0)
-            plotWithLines()
+        // Destaca o x0 e y0
+        highlightP0(data.first(), 0.0)
+    }
+
+    override fun dataToElementsToPlot(): List<Node> {
+        val coords = (listOf(Pair(data[0], 0.0)) + (1 until data.size)
+                .flatMap { i -> listOf(Pair(data[i - 1], data[i]), Pair(data[i], data[i])) })
+                .map { (x, y) -> Pair(x.realToChartX(), y.realToChartY()) }
+
+        return coordinatesToLines(coords) { l, i ->
+            l.stroke = getRainbowColor(i.toDouble() / coords.size)
+            l.strokeWidth = (DELTA_WIDTH * i / coords.size + MIN_WIDTH)
+            l.opacity = 0.5
         }
     }
 
-//    override fun p0Moved(square: Point0, pos: Point2D) {
-//        if(pos.x >= 0 && pos.x <= background.layoutBounds.maxX) {
-//            square.x = pos.x
-//        }
-//    }
+    override fun plotData(elements: List<Node>) {
+        background.getChildList()?.addAll(elements)
+    }
 
 }
 
