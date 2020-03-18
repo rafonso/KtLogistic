58ac395bf18dbd0b34c61b46a53465ae6c36c816
Rafael Afonso <rafonso@magnasistemas.com.br>
2020-03-18 10:48:33 -0300

    Generalização e abstração de BifurcationCanvas

--- src/main/kotlin/rafael/logistic/maps/bifurcation/canvas/BifurcationCanvas.kt
+++ src/main/kotlin/rafael/logistic/maps/bifurcation/canvas/BifurcationCanvas.kt
@@ -1,76 +1,18 @@
 package rafael.logistic.maps.bifurcation.canvas
 
-import javafx.beans.property.ReadOnlyDoubleProperty
-import javafx.beans.property.ReadOnlyObjectProperty
-import javafx.event.EventHandler
-import javafx.geometry.Point2D
-import javafx.scene.canvas.Canvas
-import javafx.scene.paint.Color
 import rafael.logistic.maps.bifurcation.RData
-import rafael.logistic.view.GenerationStatus
 import rafael.logistic.view.getRainbowColor
-import rafael.logistic.view.mapchart.MapChart
-import tornadofx.*
+import rafael.logistic.view.mapchart.CanvasChart
+import rafael.logistic.view.mapchart.PixelInfo
 import java.util.stream.Stream
 import kotlin.streams.toList
 
-typealias PixelInfo = Triple<Int, Int, Color>
-
-class BifurcationCanvas : Canvas(), MapChart<RData, PixelInfo> {
+class BifurcationCanvas : CanvasChart<RData>() {
 
     // @formatter:off
 
-             val    x0Property                  =   (0.0).toProperty()
-    private  val    x0                          by  x0Property
-
-    override val    xMinProperty                =   (0.0).toProperty()
-    override val    xMin                        by  xMinProperty
-
-    override val    xMaxProperty                =   (0.0).toProperty()
-    override val    xMax                        by  xMaxProperty
-
-    override val    yMinProperty                =   (0.0).toProperty()
-    override val    yMin                        by  yMinProperty
-
-    override val    yMaxProperty                =   (0.0).toProperty()
-    override val    yMax                        by  yMaxProperty
-
-    private     val deltaXByPixelProp           =   (0.0).toProperty()
-    override    val deltaXByPixelProperty       =   deltaXByPixelProp as ReadOnlyDoubleProperty
-
-    private     val deltaYByPixelProp           =   (0.0).toProperty()
-    override    val deltaYByPixelProperty       =   deltaYByPixelProp as ReadOnlyDoubleProperty
-
-    override    val generationStatusProperty    =   GenerationStatus.IDLE.toProperty()
-
-    private     val dataProperty                =   emptyList<RData>().toProperty()
-    private     var data                        by  dataProperty
-
-    private     val mousePositionRealProperty   =   Point2D(0.0, 0.0).toProperty()
 
     // @formatter:on
-
-    private fun Double.realToCanvasX() = (this - xMin) / (xMax - xMin) * super.getWidth()
-
-    private fun Double.canvasToRealX() = (xMax - xMin) * this / super.getWidth() + xMin
-
-    private fun Double.realToCanvasY() = (1 - (this - yMin) / (yMax - yMin)) * super.getHeight()
-
-    private fun Double.canvasToRealY() = (yMax - yMin) * (super.getHeight() - this) / super.getHeight() + yMin
-
-    private val gc = super.getGraphicsContext2D()
-
-    init {
-        deltaXByPixelProp.bind((xMaxProperty - xMinProperty) / super.widthProperty())
-        deltaYByPixelProp.bind((yMaxProperty - yMinProperty) / super.heightProperty())
-
-        dataProperty.onChange { refreshData() }
-
-        this.onMouseMoved = EventHandler { event ->
-            mousePositionRealProperty.value = Point2D(event.x.canvasToRealX(), event.y.canvasToRealY())
-        }
-        this.onMouseExited = EventHandler { mousePositionRealProperty.value = null }
-    }
 
     private fun rSequenceToCoordinates(rSequence: RData): Stream<PixelInfo> {
         val rChart = rSequence.r.realToCanvasX().toInt()
@@ -82,28 +24,8 @@ class BifurcationCanvas : Canvas(), MapChart<RData, PixelInfo> {
                 .map { Triple(rChart, it.first, getRainbowColor(it.second)) }
     }
 
-    override fun prepareBackground() {
-        gc.clearRect(0.0, 0.0, width, height)
-
-        // Paint background
-        gc.fill = Color.WHITE
-        gc.fillRect(0.0, 0.0, width, height)
-    }
-
     override fun dataToElementsToPlot() = data
             .parallelStream()
             .flatMap(this::rSequenceToCoordinates).toList()
-
-    override fun plotData(elements: List<PixelInfo>) {
-        val pixelWriter = gc.pixelWriter
-        elements.forEach { (x, y, c) -> pixelWriter.setColor(x, y, c) }
-    }
-
-    override fun mousePositionRealProperty() = mousePositionRealProperty as ReadOnlyObjectProperty<Point2D>
-
-    override fun bind(dataProperty: ReadOnlyObjectProperty<List<RData>>, handler: (MapChart<RData, *>) -> Unit) {
-        this.dataProperty.bind(dataProperty)
-        handler(this)
-    }
 
 }
---
+++ src/main/kotlin/rafael/logistic/view/mapchart/CanvasChart.kt
@@ -0,0 +1,92 @@
+package rafael.logistic.view.mapchart
+
+import javafx.beans.property.ReadOnlyDoubleProperty
+import javafx.beans.property.ReadOnlyObjectProperty
+import javafx.event.EventHandler
+import javafx.geometry.Point2D
+import javafx.scene.canvas.Canvas
+import javafx.scene.paint.Color
+import rafael.logistic.view.GenerationStatus
+import tornadofx.*
+
+typealias PixelInfo = Triple<Int, Int, Color>
+
+abstract class CanvasChart<T> : Canvas(), MapChart<T, PixelInfo> {
+
+    // @formatter:off
+
+                val x0Property                  =   (0.0).toProperty()
+    private     val x0                          by  x0Property
+
+    override    val xMinProperty                =   (0.0).toProperty()
+    override    val xMin                        by  xMinProperty
+
+    override    val xMaxProperty                =   (0.0).toProperty()
+    override    val xMax                        by  xMaxProperty
+
+    override    val yMinProperty                =   (0.0).toProperty()
+    override    val yMin                        by  yMinProperty
+
+    override    val yMaxProperty                =   (0.0).toProperty()
+    override    val yMax                        by  yMaxProperty
+
+    private     val deltaXByPixelProp           =   (0.0).toProperty()
+    override    val deltaXByPixelProperty       =   deltaXByPixelProp as ReadOnlyDoubleProperty
+
+    private     val deltaYByPixelProp           =   (0.0).toProperty()
+    override    val deltaYByPixelProperty       =   deltaYByPixelProp as ReadOnlyDoubleProperty
+
+    override    val generationStatusProperty    =   GenerationStatus.IDLE.toProperty()
+
+    private     val dataProperty                =   emptyList<T>().toProperty()
+    protected   var data :List<T>               by  dataProperty
+
+    private     val mousePositionRealProperty   =   Point2D(0.0, 0.0).toProperty()
+
+                val backgroundProperty          =   Color.WHITE.toProperty()
+
+    // @formatter:on
+
+    protected fun Double.realToCanvasX() = (this - xMin) / (xMax - xMin) * super.getWidth()
+
+    private fun Double.canvasToRealX() = (xMax - xMin) * this / super.getWidth() + xMin
+
+    protected fun Double.realToCanvasY() = (1 - (this - yMin) / (yMax - yMin)) * super.getHeight()
+
+    private fun Double.canvasToRealY() = (yMax - yMin) * (super.getHeight() - this) / super.getHeight() + yMin
+
+    private val gc = super.getGraphicsContext2D()
+
+    init {
+        deltaXByPixelProp.bind((xMaxProperty - xMinProperty) / super.widthProperty())
+        deltaYByPixelProp.bind((yMaxProperty - yMinProperty) / super.heightProperty())
+
+        dataProperty.onChange { refreshData() }
+
+        this.onMouseMoved = EventHandler { event ->
+            mousePositionRealProperty.value = Point2D(event.x.canvasToRealX(), event.y.canvasToRealY())
+        }
+        this.onMouseExited = EventHandler { mousePositionRealProperty.value = null }
+    }
+
+    override fun prepareBackground() {
+        gc.clearRect(0.0, 0.0, width, height)
+
+        // Paint background
+        gc.fill = backgroundProperty.value
+        gc.fillRect(0.0, 0.0, width, height)
+    }
+
+    override fun plotData(elements: List<PixelInfo>) {
+        val pixelWriter = gc.pixelWriter
+        elements.forEach { (x, y, c) -> pixelWriter.setColor(x, y, c) }
+    }
+
+    override fun mousePositionRealProperty() = mousePositionRealProperty as ReadOnlyObjectProperty<Point2D>
+
+    override fun bind(dataProperty: ReadOnlyObjectProperty<List<T>>, handler: (MapChart<T, *>) -> Unit) {
+        this.dataProperty.bind(dataProperty)
+        handler(this)
+    }
+
+}
