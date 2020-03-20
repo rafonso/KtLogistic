5c7beff3ae7d36ebbd67b6a6999c413f487d4781
Rafael Afonso <rafonso@magnasistemas.com.br>
2020-03-20 08:37:24 -0300

    Simplificação da Convergência.

--- src/main/kotlin/rafael/logistic/maps/bifurcation/ConvergenceVerifier.kt
+++ src/main/kotlin/rafael/logistic/maps/bifurcation/ConvergenceVerifier.kt
@@ -9,8 +9,6 @@ sealed class ConvergenceVerifier {
     companion object {
 
         fun valueOf(convergenceType: ConvergenceType, r: Double) = when (convergenceType) {
-            ConvergenceType.ZERO     -> ZeroVerifier
-            ConvergenceType.CONSTANT -> ConstantVerifier(r)
             ConvergenceType.CYCLE_2  -> Cycle2Verifier
             ConvergenceType.CHAOS    -> ChaosVerifier
         }
@@ -18,18 +16,6 @@ sealed class ConvergenceVerifier {
     }
 
     abstract fun converges(values: Data): Boolean
-
-}
-
-object ZeroVerifier : ConvergenceVerifier() {
-    override fun converges(values: Data): Boolean = values.last() <= CONVERGENCE_DIFF
-}
-
-class ConstantVerifier(r: Double) : ConvergenceVerifier() {
-
-    private val convergenceValue = (r - 1) / r
-
-    override fun converges(values: Data): Boolean = abs(values.last() - convergenceValue) <= CONVERGENCE_DIFF
 
 }
 
