2fffcdb62749fc0d8dbd8cd94fee6dbfa76d81e0
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2020-04-06 14:20:24 -0300

    Código para GenerationStatus.

--- ktfractal-core/src/main/kotlin/rafael/logistic/core/generation/GenerationStatus.kt
+++ ktfractal-core/src/main/kotlin/rafael/logistic/core/generation/GenerationStatus.kt
@@ -1,10 +1,10 @@
 package rafael.logistic.core.generation
 
-enum class GenerationStatus {
-    IDLE,
-    CALCULATING,
-    PLOTTING_PREPARING,
-    PLOTTING_CONVERT,
-    PLOTTING_DRAW,
-    PLOTTING_FINALIZING
+enum class GenerationStatus(val code: String) {
+    IDLE                ("ID"),
+    CALCULATING         ("CA"),
+    PLOTTING_PREPARING  ("PP"),
+    PLOTTING_CONVERT    ("PC"),
+    PLOTTING_DRAW       ("PD"),
+    PLOTTING_FINALIZING ("PF")
 }
