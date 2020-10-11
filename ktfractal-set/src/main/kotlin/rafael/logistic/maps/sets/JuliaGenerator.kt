12a97df95d0e4824aee96dea4a632d74bde6401d
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2020-10-11 03:54:32 -0300

    Optimization for Julia Generation

--- ktfractal-set/src/main/kotlin/rafael/logistic/maps/sets/JuliaGenerator.kt
+++ ktfractal-set/src/main/kotlin/rafael/logistic/maps/sets/JuliaGenerator.kt
@@ -5,25 +5,77 @@ import rafael.logistic.core.generation.IterationGenerator
 import rafael.logistic.core.generation.IterationParameter
 import java.util.stream.Collectors
 
-data class JuliaParameter(val cX: Double, val cY: Double,
-                          val xMin: Double, val xMax: Double, val width: Int,
-                          val yMin: Double, val yMax: Double, val height: Int) : IterationParameter {
+data class JuliaParameter(
+    val cX: Double, val cY: Double,
+    val xMin: Double, val xMax: Double, val width: Int,
+    val yMin: Double, val yMax: Double, val height: Int
+) : IterationParameter {
 
     private val deltaX = (xMax - xMin) / width
 
     private val deltaY = (yMax - yMin) / height
 
     val xValues = (0..width).map { it * deltaX + xMin }
+    internal val xValuesArray by lazy { xValues.toDoubleArray() }
     val cols = xValues.indices.toList()
 
     val yValues = (0..height).map { it * deltaY + yMin }
+    internal val yValuesArray  by lazy { yValues.toDoubleArray() }
     val rows = yValues.indices.toList()
 
+    val valuesToBeProcessed = xValues.size * yValues.size
 }
 
 abstract class JuliaGenerator : IterationGenerator<BiDouble, JuliaInfo, JuliaParameter> {
 
-    protected tailrec fun checkConvergence(zx: Double, zy: Double, cx: Double, cy: Double, convergenceSteps: Int, iteration: Int = 1): Int? {
+    private fun generateParallel(parameter: JuliaParameter, interactions: Int): List<JuliaInfo> {
+        return parameter.cols.parallelStream()
+            .flatMap { col ->
+                val x = parameter.xValues[col]
+                parameter.rows.parallelStream()
+                    .map { row ->
+                        val y = parameter.yValues[row]
+                        JuliaInfo(
+                            col,
+                            row,
+                            x,
+                            y,
+                            verify(x, y, parameter, interactions)
+                        )
+                    }
+                    .filter { ji -> !ji.converges }
+            }.collect(Collectors.toList())
+    }
+
+    private fun generateFromArray(parameter: JuliaParameter, interactions: Int): List<JuliaInfo> {
+        val result = Array(parameter.xValues.size * parameter.yValues.size) { emptyJuliaInfo }
+        var i = 0
+
+        parameter.xValuesArray.forEachIndexed { col, x ->
+            parameter.yValuesArray.forEachIndexed { row, y ->
+                result[i] =
+                    JuliaInfo(
+                        col,
+                        row,
+                        x,
+                        y,
+                        verify(x, y, parameter, interactions)
+                    )
+                i++
+            }
+        }
+
+        return result.filter { ji -> !ji.converges }
+    }
+
+    protected tailrec fun checkConvergence(
+        zx: Double,
+        zy: Double,
+        cx: Double,
+        cy: Double,
+        convergenceSteps: Int,
+        iteration: Int = 1
+    ): Int? {
         if (iteration == convergenceSteps) {
             return null
         }
@@ -37,23 +89,9 @@ abstract class JuliaGenerator : IterationGenerator<BiDouble, JuliaInfo, JuliaPar
 
     protected abstract fun verify(x: Double, y: Double, parameter: JuliaParameter, interactions: Int): Int?
 
-    override fun generate(z0: BiDouble, parameter: JuliaParameter, interactions: Int): List<JuliaInfo> {
-        return parameter.cols.parallelStream()
-                .flatMap { col ->
-                    val x = parameter.xValues[col]
-                    parameter.rows.parallelStream()
-                            .map { row ->
-                                val y = parameter.yValues[row]
-                                JuliaInfo(
-                                    col,
-                                    row,
-                                    x,
-                                    y,
-                                    verify(x, y, parameter, interactions)
-                                )
-                            }
-                            .filter { ji -> !ji.converges }
-                }.collect(Collectors.toList())
-    }
+    @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
+    override fun generate(z0: BiDouble, parameter: JuliaParameter, interactions: Int): List<JuliaInfo> =
+        if ((parameter.valuesToBeProcessed) <= 5000) generateFromArray(parameter, interactions)
+        else generateParallel(parameter, interactions)
 
 }
--- ktfractal-set/src/main/kotlin/rafael/logistic/maps/sets/JuliaInfo.kt
+++ ktfractal-set/src/main/kotlin/rafael/logistic/maps/sets/JuliaInfo.kt
@@ -5,3 +5,5 @@ data class JuliaInfo(val col: Int, val row: Int, val x: Double, val y: Double, v
     val converges = (iterationsToDiverge == null)
 
 }
+
+val emptyJuliaInfo = JuliaInfo(Int.MIN_VALUE, Int.MIN_VALUE, Double.NaN, Double.NaN, Int.MIN_VALUE)
