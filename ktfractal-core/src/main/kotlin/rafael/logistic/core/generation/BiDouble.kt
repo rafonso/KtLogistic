82fdacc43ecdb0e734e8813c4f2c0a087f43c9a3
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2020-10-16 08:45:00 -0300

    Documentação.

--- ktfractal-core/src/main/kotlin/rafael/logistic/core/generation/BiDouble.kt
+++ ktfractal-core/src/main/kotlin/rafael/logistic/core/generation/BiDouble.kt
@@ -6,7 +6,14 @@ package rafael.logistic.core.generation
 data class BiDouble(val x: Double, val y: Double) {
 
     companion object {
+        /**
+         * [BiDouble] composto por dois 0s
+         */
         val ZERO = BiDouble(0.0, 0.0)
+
+        /**
+         * [BiDouble] composto por dois [Double.NaN]
+         */
         val NAN = BiDouble(Double.NaN, Double.NaN)
     }
 
