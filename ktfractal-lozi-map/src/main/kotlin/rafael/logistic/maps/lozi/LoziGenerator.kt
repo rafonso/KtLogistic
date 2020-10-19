3158e030a4f0d03f4128cca6aa2a6529c3cb1ebc
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2020-10-19 15:59:57 -0300

    Lozi Bifurcation

--- ktfractal-lozi-map/src/main/kotlin/rafael/logistic/maps/lozi/LoziGenerator.kt
+++ ktfractal-lozi-map/src/main/kotlin/rafael/logistic/maps/lozi/LoziGenerator.kt
@@ -7,12 +7,16 @@ import kotlin.math.absoluteValue
 
 data class LoziParameter(val alpha: Double, val beta: Double) : IterationParameter
 
+// https://mathworld.wolfram.com/LoziMap.html
 class LoziGenerator : IterationGeneratorBi<LoziParameter>() {
 
-    override fun calculate(parameter: LoziParameter, point: BiDouble): BiDouble =
-            BiDouble(1.0 - parameter.alpha * point.x.absoluteValue + point.y, parameter.beta * point.x)
+    override fun calculate(
+        parameter: LoziParameter,
+        @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE") point: BiDouble
+    ): BiDouble =
+        BiDouble(1.0 - parameter.alpha * point.x.absoluteValue + point.y, parameter.beta * point.x)
 
     fun generate(p0: BiDouble, alpha: Double, beta: Double, iterations: Int) =
-            super.generate(p0, LoziParameter(alpha, beta), iterations)
+        super.generate(p0, LoziParameter(alpha, beta), iterations)
 
 }
