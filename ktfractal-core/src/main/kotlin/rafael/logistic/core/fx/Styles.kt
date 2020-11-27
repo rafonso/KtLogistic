2a68d71b04836d40adcd969b816f9b11e52b7c6c
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2020-11-27 10:04:55 +0000

    Refactoring styles

--- ktfractal-core/src/main/kotlin/rafael/logistic/core/fx/Styles.kt
+++ ktfractal-core/src/main/kotlin/rafael/logistic/core/fx/Styles.kt
@@ -9,6 +9,10 @@ class Styles : Stylesheet() {
     companion object {
         val title by cssclass()
         val mouseRealPosNode by csselement("MouseRealPosNode")
+        val spnDouble by cssclass("spnDouble")
+        val spnIterations by cssid("spnIterations")
+        val spnInteger by cssclass("spnInteger")
+        val lblStatus by cssclass("status")
     }
 
     init {
@@ -23,12 +27,25 @@ class Styles : Stylesheet() {
         label and title {
             alignment = Pos.CENTER_RIGHT
             minWidth = 75.px
+            padding = box(0.px, 5.px, 0.px, 0.px)
         }
         mouseRealPosNode {
             font = Font.font("Consolas", 10.0)
             insets(5.0)
             prefWidth = 200.px
             maxWidth = 200.px
+        }
+        spinner and spnDouble {
+            prefWidth = 120.px
+        }
+        spinner and spnIterations {
+            prefWidth = 100.px
+        }
+        spinner and spnInteger {
+            prefWidth = 60.px
+        }
+        label and lblStatus {
+            prefWidth = 200.px
         }
     }
 }
