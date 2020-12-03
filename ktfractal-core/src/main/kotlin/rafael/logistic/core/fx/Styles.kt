95bbe39557c0fb8435f5d359a8e0612758622135
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2020-12-03 15:21:09 +0000

    Refatoração de styles.

--- ktfractal-core/src/main/kotlin/rafael/logistic/core/fx/Styles.kt
+++ ktfractal-core/src/main/kotlin/rafael/logistic/core/fx/Styles.kt
@@ -6,39 +6,38 @@ import javafx.scene.paint.Color
 import javafx.scene.text.Font
 import tornadofx.*
 
+/**
+ * Configuração CSS
+ */
 class Styles : Stylesheet() {
     companion object {
-        val pnlControls by cssclass("controls")
-        val title by cssclass()
-        val mouseRealPosNode by csselement("MouseRealPosNode")
-        val spnDouble by cssclass("spnDouble")
-        val spnIterations by cssid("spnIterations")
-        val spnInteger by cssclass("spnInteger")
-        val lblStatus by cssclass("status")
-        val pnlBottom by cssclass("bottom")
-        val defaultInsets by cssclass("defaultInsets")
+        // @formatter:off
+        val pnlControls         by cssclass     ("controls"         )
+        val title               by cssclass     ()
+        val mouseRealPosNode    by csselement   ("MouseRealPosNode" )
+        val spnDouble           by cssclass     ("spnDouble"        )
+        val spnIterations       by cssid        ("spnIterations"    )
+        val spnInteger          by cssclass     ("spnInteger"       )
+        val lblStatus           by cssclass     ("status"           )
+        val pnlBottom           by cssclass     ("bottom"           )
+        val defaultInsets       by cssclass     ("defaultInsets"    )
+    // @formatter:on
     }
 
     init {
-        spinner and focused {
-            textInput {
-                backgroundColor += Color.KHAKI
-            }
-        }
-        chartPlotBackground {
-            backgroundColor += Color.WHITE
-        }
         label and title {
             alignment = Pos.CENTER_RIGHT
             minWidth = 75.px
 //            prefWidth = 100.px
             padding = box(0.px, 5.px, 0.px, 0.px)
         }
-        mouseRealPosNode {
-            font = Font.font("Consolas", 10.0)
-            insets(5.0)
+        label and lblStatus {
             prefWidth = 200.px
-            maxWidth = 200.px
+        }
+        spinner and focused {
+            textInput {
+                backgroundColor += Color.KHAKI
+            }
         }
         spinner and spnDouble {
             prefWidth = 120.px
@@ -55,8 +54,17 @@ class Styles : Stylesheet() {
                 alignment = Pos.CENTER_RIGHT
             }
         }
-        label and lblStatus {
+        mouseRealPosNode {
+            font = Font.font("Consolas", 10.0)
+            insets(5.0)
             prefWidth = 200.px
+            maxWidth = 200.px
+        }
+        chartPlotBackground {
+            backgroundColor += Color.WHITE
+        }
+        defaultInsets {
+            padding = box(5.px)
         }
         pnlBottom {
             hgap = 5.px
@@ -64,9 +72,6 @@ class Styles : Stylesheet() {
             borderWidth += box(1.px)
             borderStyle = multi(BorderStrokeStyle.DASHED)
             // style="-fx-border-width: 1px; -fx-border-style: dashed;" vgap="5.0"
-        }
-        defaultInsets {
-            padding = box(5.px)
         }
         pnlControls {
             vgap = 5.px
